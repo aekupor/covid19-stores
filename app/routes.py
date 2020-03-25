b7e20f00b53ddffef51093b2cbc934bebc16e522
Ashlee Kupor <ashleekupor@Ashlees-MacBook-Pro.local>
2020-03-25 10:56:59 -0700

    Created hello world application with login page and home page. Used Flask basics to run on localhost.

---
+++ app/routes.py
@@ -0,0 +1,25 @@
+from flask import render_template
+from app import app
+from app.forms import LoginForm
+
+
+@app.route('/')
+@app.route('/index')
+def index():
+    user = {'username': 'Ashlee'}
+    posts = [
+        {
+            'author': {'username': 'Mark'},
+            'body': 'Safeway now has eggs in stock!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'Trader Joes is out of bread!'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Sign In', form=form)
