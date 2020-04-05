137b575bc8dbbae39326efbe3299bee38d9c29bc
Ashlee <aekupor@gmail.com>
2020-04-05 12:23:51 -0700

    Added dropdown menu to choose store for post.

--- app/forms.py
+++ app/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextAreaField
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextAreaField, SelectField
 from wtforms.validators import ValidationError, DataRequired, Email, EqualTo, Length
 from app.models import User
 
@@ -30,8 +30,7 @@ class RegistrationForm(FlaskForm):
 class PostForm(FlaskForm):
     post = TextAreaField('Say something', validators=[
         DataRequired(), Length(min=1, max=140)])
-    store = TextAreaField('Store', validators=[
-        DataRequired(), Length(min=1, max=140)])
+    store = SelectField('Store', validators=[DataRequired()], choices=[("Trader Joe's","Trader Joe's"), ('Safeway', 'Safeway')])
     submit = SubmitField('Submit')
 
 class EditProfileForm(FlaskForm):
